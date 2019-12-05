68bed518ab19b0906abf731f0c3ba801aed3c927
Jan Soons <jsoons@gmail.com>
2019-12-05 15:51:00 +0100

    nodes try1

--- html/test3.js
+++ html/test3.js
@@ -10,4 +10,19 @@ var x = document.getElementsByTagName("p");
 document.write(x);
 document.write(x[1].innerText);
 
+document.getElementById("p1").style.color=("blue");
+function displayalert() {
+    alert("button clicked");
+}
+
+document.getElementById("newButton").addEventListener("click",displayalert);
+
+function printNodeName(n) {
+    console.log(n.printNodeName);
+}
+
+rn = document.getRootNode();
+console.log(rn.printNodeName);
+printNodeName(rn);
+
 
