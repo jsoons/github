5e25dd6459fd42bb009c7b8b0d1f8eece4f2d54f
jsoons <jsoons@gmail.com>
2019-12-05 14:20:15 +0100

    10digit

--- html/test2.js
+++ html/test2.js
@@ -1,1 +1,19 @@
 document.write("Hello World.");
+var number = "1234567890";
+try {
+  if (number.length !=10) {
+    throw ("Please enter a 10-digit number!");
+  } else {
+    document.write(number);
+  }
+   
+
+} catch (e) {
+  alert ("Error "+e);
+} finally {
+  document.write("</BR> This is always executed.");
+}
+
+var text = "I like an apple...";
+pat = /apple/;
+document.write(text.replace(pat,"pie"));
